f1aecfb30f94d01871d85db541ae57294b2bb279
Esther Witbeck <esther.witbeck@gmail.com>
2023-04-19 15:52:40 -0700

    fill out bookCreator react component

--- src/app/components/mainContainer.jsx
+++ src/app/components/mainContainer.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-/* import PendingWindow from './pendingWindow.jsx' */
-/* import BookCreator from './bookCreator.jsx' */
-/* import CompletedWindow from './completedWindow.jsx' */
+import PendingWindow from './pendingWindow.jsx';
+import BookCreator from './bookCreator.jsx';
+import CompletedWindow from './completedWindow.jsx';
 
 export default () => (
   <main id='mainContainer'> 
-    <section id='pendingWindow'></section>
-    <div id='bookCreator'></div>
-    <section id='completedWindow'></section>
+    <PendingWindow />
+    <BookCreator />
+    <CompletedWindow />
   </main>
 );
 
