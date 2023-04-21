a9cb43cffc2a33def94bb0b88856a7159895c55a
Esther Witbeck <esther.witbeck@gmail.com>
2023-04-20 17:17:21 -0700

    add focus to popup window

--- src/app/components/popupContainer.jsx
+++ src/app/components/popupContainer.jsx
@@ -6,16 +6,27 @@ export default props => {
   const selection = useSelector(state => state.appState.selection);
   const { title, year, author } = selection;
   const dispatch = useDispatch();
-  const close = () => {
+  const close = e => {
+    e.preventDefault();
     dispatch(toggle());
   };
+
+  const clickFocus = e => {
+    const div = document.getElementById('popupContainer');
+    div.focus();
+  }
 
   const queryString = (title ? `${title}, ` : '') +
                       (author ? author.replace(/(.*), ?(.*)/, '$2 $1, ') : '') +
                       (year ? year : '');
 
   return (
-    <div id='popupContainer'>
+    <div
+      id='popupContainer'
+      tabIndex='-1'
+      onBlur={close}
+      onClick={clickFocus}
+    >
       <h4>{queryString}</h4>
       <h4>Bibliography</h4>
       <form>
